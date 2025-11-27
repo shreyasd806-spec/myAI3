3dea3cf44923df3412fe3161e20fa6c19106a57d
shreyasd806-spec <shreyasd806@gmail.com>
2025-11-27 20:06:47 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Inter } from "next/font/google";
-import { ThemeToggle } from "./components/theme-toggle";
+import  ThemeToggle  from "./components/theme-toggle";
 
 const inter = Inter({ subsets: ["latin"] });
 
